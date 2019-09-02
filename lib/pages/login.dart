00dfeba7d2c2648c807407f5162509c812c25fde
Band Ly <bandly93@gmail.com>
2019-09-01 22:11:59 -0700

    add login page ui

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/common/navBar.dart';
+import 'package:flutter_app/common/button.dart';
 
 LoginPage(){
   return Scaffold(
     body : Column(
       children : <Widget>[
         NavBar(),
+        Container(
+          child : Column(
+            children: <Widget>[
+              Text("Login"),
+              TextField(decoration: InputDecoration(labelText: 'Email'),),
+              TextField(decoration: InputDecoration(labelText: 'Password'),),
+              Button(text : "Login"),
+              Text("Forget Password?")
+            ],
+          )
+        )
       ]
     )  
   );
